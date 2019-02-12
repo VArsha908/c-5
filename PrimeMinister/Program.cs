04f5839c1ad2fcc89d866ff4b22f35ce01c71902
Varsha Surana <varshasurana09@gmail.com>
2019-02-12 12:55:12 +0530

    assignment 5

---
+++ PrimeMinister/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeMinister
+{
+    class Program
+    {
+        static void Main(string[] args)
+            {
+                Dictionary<int, string> PrimeMinisters = new Dictionary<int, string>();
+                PrimeMinisters = new Dictionary<int, string>()
+                {
+                    { 1998 , "Atal Bihari Vajpayee" },
+                    { 2014 , "Narendra Modi"},
+                    { 2004 , "Manmohan Singh"}
+                };
+                if (PrimeMinisters.ContainsKey(2004))
+                {
+                    Console.WriteLine("The Prime Minister of 2004 is:" + PrimeMinisters[2004]);
+                }
+
+                PrimeMinisters.Add(2019, "Narendra Modi");
+                Console.WriteLine("After adding the list is here:");
+            foreach (var item in PrimeMinisters)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("After sorting the list is here:");
+            var pm = PrimeMinisters.Keys.ToList();
+            pm.Sort();
+            foreach (var keys in pm)
+            { 
+                Console.WriteLine(keys + "\t" + PrimeMinisters[keys]);
+            }
+
+
+        }
+        }
+    }
